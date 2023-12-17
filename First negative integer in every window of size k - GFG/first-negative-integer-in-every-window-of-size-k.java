907919be7b4c6ff441d0c1cab7e4c47f2891b4b4
Rishab jha <134808884+rishabjha03@users.noreply.github.com>
2023-12-17 21:52:23 +0530

    Added solution - LeetHub

---
+++ First negative integer in every window of size k - GFG/first-negative-integer-in-every-window-of-size-k.java
@@ -0,0 +1,80 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main(String[] args) throws IOException
+	{
+	        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+        int t =
+            Integer.parseInt(br.readLine().trim()); // Inputting the testcases
+        while(t-->0)
+        {
+            int n = Integer.parseInt(br.readLine().trim());
+            long a[] = new long[(int)(n)];
+            // long getAnswer[] = new long[(int)(n)];
+            String inputLine[] = br.readLine().trim().split(" ");
+            for (int i = 0; i < n; i++) {
+                a[i] = Long.parseLong(inputLine[i]);
+            }
+            int k = Integer.parseInt(br.readLine().trim());
+            
+            Compute obj = new Compute();
+            long answer[] = obj.printFirstNegativeInteger(a, n, k);
+            int sz = answer.length;
+            
+            StringBuilder output = new StringBuilder();
+            for(int i=0;i<sz;i++)
+                output.append(answer[i]+" ");
+            System.out.println(output);
+            
+        }
+	}
+}
+
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+
+class Compute {
+    
+    public long[] printFirstNegativeInteger(long A[], int N, int K)
+    {
+        long res[] = new long[N-K+1];
+        Queue<Integer> q = new LinkedList<>();
+        
+        //step1:-
+        for(int i=0;i<N;i++){
+            if(A[i]<0){
+                q.add(i);
+            }
+        }
+        
+        //step2:-
+        for(int i=0;i<N-K+1;i++){
+            if(q.size()>0 && q.peek()<i){
+                q.remove();
+                
+            } 
+            if(q.size()>0 && q.peek()<=i+K-1){
+                res[i] = A[q.peek()];
+            }
+            else if(q.size()==0){
+                res[i] =0;
+            } else{
+                res[i] = 0;
+            }
+        }
+        
+        return res;
+        
+        
+    }
+}
